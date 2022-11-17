65eaa0fb0529a34ca6c74e4311afecc5550b8eea
ravindrahv <ravindrahvasudeva@gmail.com>
2022-11-17 21:35:36 +0530

    codes

---
+++ codes/com.code.ravi/Fibonacci.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int fib(int n)
+{
+    int f[n + 2];
+    int i;
+ 
+ 
+    f[0] = 0;
+    f[1] = 1;
+ 
+    for(i = 2; i <= n; i++)
+    {
+       f[i] = f[i - 1] + f[i - 2];
+    }
+    return f[n];
+    }
+ 
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<fib(n);
+    return 0;
+	}
