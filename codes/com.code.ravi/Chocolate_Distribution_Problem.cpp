528143c57fcfe9ea1bf816b853e8f3ccabbb3091
ravindrahv <ravindrahvasudeva@gmail.com>
2022-11-29 21:25:24 +0530

    codes

--- codes/com.code.ravi/Chocolate_Distribution_Problem.cpp
+++ codes/com.code.ravi/Chocolate_Distribution_Problem.cpp
@@ -32,3 +32,14 @@ int main()
          << findMinDiff(arr, n, m);
     return 0;
 }
+int main()
+{
+    int arr[] = { 12, 4,  7,  9,  2,  23, 25, 41, 30,
+                  40, 28, 42, 30, 44, 48, 43, 50 };
+    int m = 7; 
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Minimum difference is "
+         << findMinDiff(arr, n, m);
+    return 0;
+}
+ 
