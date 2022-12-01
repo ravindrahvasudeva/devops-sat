722584dac216c098f5994ac65ef779c073a5c477
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-01 22:25:26 +0530

    codes

---
+++ codes/com.code.ravi/StringReverse.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void reverse(string str)
+{
+    int len = str.length();
+    int n = len;
+    while(n--)
+        cout << str[n];
+}
+ 
+
+int main(void)
+{
+    string s = "Ravindrahv";
+ 
+    reverse(s);
+    return (0);
+}
