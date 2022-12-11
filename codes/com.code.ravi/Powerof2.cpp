9af699f56ce723362dae5a3e40330c57ed30af7c
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-11 23:24:14 +0530

    codes

--- codes/com.code.ravi/Powerof2.cpp
+++ codes/com.code.ravi/Powerof2.cpp
@@ -1,6 +1,7 @@
+/*Given an integer n, return true if it is a power of two. Otherwise, return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.*/
 #include <bits/stdc++.h>
 using namespace std;
-
 bool Po2(int num){
     int n = 1;
     for(int i=0;i<=30;i++){
@@ -10,8 +11,6 @@ bool Po2(int num){
         n=n*2;
     }return false;
 }
-
-
 int main()
 {
 	int num = 1073741824;
